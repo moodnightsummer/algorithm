2eb4e3cc28076f2cacde6271d0da8c042ff934c5
moodnightsummer <71811826+moodnightsummer@users.noreply.github.com>
2023-12-11 20:05:52 +0900

    [Bronze II] Title: 화성 수학, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5355. 화성 수학/화성 수학.py
@@ -0,0 +1,20 @@
+def calculator(number, items):
+    result = number
+    for i in items:
+        if(i == "@"):
+            result = result * 3
+        elif(i == "%"):
+            result = result + 5
+        elif(i == "#"):
+            result = result - 7
+
+    return result;
+
+
+testCase = int(input())
+result = []
+
+for i in range(testCase):
+    array = list(input().split())
+    number = float(array.pop(0))
+    print("%0.2f" % calculator(number, array))
