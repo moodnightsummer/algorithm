8d62a85f090fdd1061e2474d306b5ff18e33ff3d
moodnightsummer <71811826+moodnightsummer@users.noreply.github.com>
2023-12-13 15:09:19 +0900

    [Bronze IV] Title: 평균 점수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.py
@@ -0,0 +1,7 @@
+one = max(40, int(input()))
+two = max(40, int(input()))
+three = max(40, int(input()))
+four = max(40, int(input()))
+five = max(40, int(input()))
+
+print((one+two+three+four+five) // 5)
