3121aaa71136c598d69a6f16ae7b962201b7fe7f
jelly <moodnightsummer@naver.com>
2025-11-17 22:48:39 +0900

    파이썬 문법

---
+++ 프로그래머스/0/배열의유사도/배열의유사도.py
@@ -0,0 +1,11 @@
+def solution(s1, s2):
+    answer = 0
+
+    for i in s1:
+        if i in s2:
+            answer= answer+1
+
+    return answer
+
+solution(["a", "b", "c"], ['com', 'b', 'd', 'p', 'c'])
+
